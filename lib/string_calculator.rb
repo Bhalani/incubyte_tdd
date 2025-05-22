c8e5e6f13c1917cbd39d0a4fd081346741420346
Bhalani <hiren.bhalani777@gmail.com>
2025-05-22 11:03:08 +0530

    Handle new line as delimiter and provide sum of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,14 +2,14 @@ module StringCalculator
   def self.add(numbers)
     numbers.strip!
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.match?(/^\d+$/)
+    return numbers.to_i if numbers.match?(/\A\d+\z/)
     return sum_of_parse_numbers(numbers)
   end
 
   private
 
   def self.sum_of_parse_numbers(numbers)
-    numbers.split(",").reduce(0) do |sum, number|
+    numbers.split(/,|\n/).reduce(0) do |sum, number|
       sum += number.to_i
     end
   end
