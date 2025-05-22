a53a9244c2667e68e592d05c0c22ee64876be3ee
Bhalani <hiren.bhalani777@gmail.com>
2025-05-22 10:23:29 +0530

    Add module and method to be used in rspec, write logic enough to pass the writen unit test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,8 @@
+require_relative "../lib/string_calculator"
+
 RSpec.describe StringCalculator do
-  describe '#sum' do
-    it 'returns 0 with empty string' do
+  describe "#sum" do
+    it "returns 0 with empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
   end
